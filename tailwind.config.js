bebea0187499a63ae8ec815162ca6a7cba0a612a
sarina <sarinaem75@gmail.com>
2024-11-21 23:35:54 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazir: ['"Vazir"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
