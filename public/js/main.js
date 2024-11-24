29e760217e6a51e40f6e067855e6979cc154f42a
sarina <sarinaem75@gmail.com>
2024-11-25 00:14:25 +0330

    add temp

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,15 @@ const dayInput = document.getElementById("days");
 const cityOutput = document.getElementById("cityNames");
 var date = new Date();
 const APIKey = "1732a9d86ff1913e09dd01beb311e578";
+var days = [
+  "یکشنبه",
+  "دوشنبه",
+  "سه‌شنبه",
+  "چهارشنبه",
+  "پنج‌شنبه",
+  "جمعه",
+  "شنبه",
+];
 
 async function getWeather() {
   var weatherResult = await (
@@ -18,24 +27,30 @@ async function getWeather() {
   ).json();
   sendInfo(weatherResult);
 }
-console.log(weatherResult);
+
+function convertToCel(value) {
+  return (value - 273).toFixed(0);
+}
 
 function sendInfo(data) {
   var cityName = data["name"];
   var temp = data.main.temp;
   var detail = {
     cityName: data["name"],
-    dateNew: date.setDate(date.getDate() + 5),
   };
-  var wind = data.wind.speed;
+  // dateNew: date.setDate(date.getDate() + 5),
+  // !
+  var wind = data["wind"]["speed"];
   var humidity = data.main.humidity;
+  console.log(data);
 
   // humidityInput
   cityOutput.innerHTML = `روزهای آینده ${cityName}`;
-  tempInput.innerHTML = temp;
+  tempInput[0].innerHTML = convertToCel(temp);
   detailInput.innerHTML = detail;
   windInput.innerHTML = wind;
   humidityInput.innerHTML = humidity;
+  date.innerHTML = detail;
 }
 
 cityNameInput.addEventListener("keydown", getWeather);
